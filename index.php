b9e7e518e6dbdea0e8ab2f9afdd5fb314d573d65
Nehueiy <nehacherry100@gmail.com>
2025-09-19 13:52:04 +0545

    index change

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $products = $res->fetch_all(MYSQLI_ASSOC);
       <img src="<?php echo htmlspecialchars($p['image']); ?>" alt="<?php echo htmlspecialchars($p['title']); ?>">
     </a>
     <h3><?php echo htmlspecialchars($p['title']); ?></h3>
-    <div>$<?php echo number_format($p['price'],2); ?></div>
+    <div>$<?php echo number_format((float)$p['price'],2); ?></div>
   </div>
 <?php endforeach; ?>
 </div>
@@ -30,31 +30,30 @@ $products = $res->fetch_all(MYSQLI_ASSOC);
 <p>No products found. Check your database.</p>
 <?php endif; ?>
 
-<?php require 'inc/footer.php'; ?>
-
 <?php
-// Assume $product['id'] is the current product being viewed
-$product_id = $product['id'];
-
-// Call Python script
-$cmd = "python recommend.py " . escapeshellarg($product_id);
-$output = shell_exec($cmd);
-
-// Decode JSON
-$recommendations = json_decode($output, true);
+// ===== Recommendations (for demo, using first product) =====
+$recommendations = [];
+if (!empty($products)) {
+    $product_id = $products[0]['id'];
+    $cmd = "python recommend.py " . escapeshellarg($product_id);
+    $output = shell_exec($cmd);
+    $recommendations = json_decode($output, true);
+}
 ?>
 
 <h3>You may also like:</h3>
 <div class="recommendations">
-  <?php if($recommendations): ?>
+  <?php if(!empty($recommendations)): ?>
     <?php foreach($recommendations as $rec): ?>
       <div class="item">
-        <img src="<?php echo $rec['image']; ?>" width="120">
-        <p><?php echo $rec['title']; ?></p>
-        <p>$<?php echo $rec['price']; ?></p>
+        <img src="<?php echo htmlspecialchars($rec['image']); ?>" width="120">
+        <p><?php echo htmlspecialchars($rec['title']); ?></p>
+        <p>$<?php echo number_format((float)$rec['price'],2); ?></p>
       </div>
     <?php endforeach; ?>
   <?php else: ?>
     <p>No recommendations found.</p>
   <?php endif; ?>
 </div>
+
+<?php require 'inc/footer.php'; ?>
